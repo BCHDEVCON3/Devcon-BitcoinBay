3c6a5457a5c89b6b3dd412886394f65c7568cf74
lightswarm124 <lightswarm@gmail.com>
2020-09-04 22:24:43 -0400

    reclaim contract balance

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const BITBOXSDK = require('bitbox-sdk');
+const { stringify } = require('@bitauth/libauth');
+
 const slpjs = require('slpjs');
-const BigNumber = require('bignumber.js');
 const {
   Contract,
   SignatureTemplate,
@@ -9,11 +10,21 @@ const {
 } = require('cashscript');
 const path = require('path');
 
-
 const BITBOX = new BITBOXSDK.BITBOX({ restURL: 'https://rest.bitcoin.com/v2/' });
 const NETWORK = "testnet";
 
 const bitboxNetwork = new slpjs.BitboxNetwork(BITBOX);
+
+let token_ticker = "bbay";
+let token_name = "bitcoinbay";
+let token_document_url = "blockhack.ca";
+let token_document_hash = null;
+let decimals = '0x00';
+let mint_baton_vout = '0x02';
+let initial_token_mint_quantity = '0x000000000000000a';
+
+let dust = 546;
+let minerFee = 2000;
 
 (async function() {
   const mnemonic = "talk story visual hidden behind wasp evil abandon bus brand circle sketch";
@@ -35,5 +46,44 @@ const bitboxNetwork = new slpjs.BitboxNetwork(BITBOX);
 
   const contract = new Contract(artifact, [alicePkh], provider);
   console.log('contract address:', contract.address);
-  console.log('contract balance:', await contract.getBalance());
+  const balance = await contract.getBalance();
+  console.log('contract balance:', balance);
+  const changeAmount = balance - dust - minerFee;
+  console.log(changeAmount);
+
+  console.log("OP Count", contract.opcount);
+  console.log("Bytesize", contract.bytesize);
+
+
+  const tx = await contract.functions
+      .reclaim(alicePk, new SignatureTemplate(keypair))
+      .to(aliceAddr, balance - 600)
+      .meep()
+/*    .genesisSLP(
+      alicePk,
+      new SignatureTemplate(keypair),
+      token_ticker,
+      token_name,
+      token_document_url,
+      decimals,
+      mint_baton_vout,
+      initial_token_mint_quantity
+    )
+    .withOpReturn([
+      '0x534c5000',
+      '0x01',
+      'GENESIS',
+      token_ticker,
+      token_name,
+      token_document_url,
+      decimals,
+      mint_baton_vout,
+      initial_token_mint_quantity
+    ])
+    .to(contract.address, dust)
+    .to(contract.address, changeAmount)
+//    .withHardcodedFee(2000)
+    .meep();
+*/
+  console.log('transaction details:', stringify(tx));
 })();
