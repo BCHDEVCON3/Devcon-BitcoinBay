0ba7528561ba6b99ce61ace422ffe91ea040da6e
lightswarm124 <lightswarm@gmail.com>
2020-09-06 17:15:40 -0400

    finalize various tx outputs for demo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const slplist = require("slp-list");
+slplist.Config.SetUrl("https://slpdb-testnet.fountainhead.cash");
+
 const BITBOXSDK = require('bitbox-sdk');
 const { stringify } = require('@bitauth/libauth');
 const BCHJS = require("@chris.troutner/bch-js")
@@ -20,13 +23,22 @@ const bitboxNetwork = new slpjs.BitboxNetwork(BITBOX);
 let token_ticker = "BCH";
 let token_name = "BCHDevCon";
 let token_document_url = "devcon.cash";
-let token_document_hash = null;
+let token_document_hash = "0xe594826a45abd156001bd6fd274ba4a79ad584d04fe3b299c5a8ea4441cf9570";
+//let token_document_hash = '';
 let decimals = '0x00';
 let mint_baton_vout = '0x02';
 let initial_token_mint_quantity = '0x000000000000000a';
+let additional_token_mint_quantity = '0x00000000000000a0';
+
 
 let dust = 546;
 let minerFee = 2000;
+
+let no_0x_slp_tx_id = "ab3af4036ce3349b9a0bb31e4b9ea7b1f7bde4137210dde3c2e3983221a1c03e";
+let slp_tx_id = "0x" + no_0x_slp_tx_id;
+
+let no_0x_frp_upload_txid = "4ae4d30c4552d3247273e871d96bc4cd7b64f926eff2277d1a9ac4ea7250e08d";
+let frp_upload_txid = "0x" + no_0x_frp_upload_txid;
 
 (async function() {
   const mnemonic = "talk story visual hidden behind wasp evil abandon bus brand circle sketch";
@@ -37,11 +49,11 @@ let minerFee = 2000;
 
   // Derive alice's public key and public key hash
   const alicePk = BITBOX.ECPair.toPublicKey(keypair);
-  console.log(alicePk);
+  console.log("Alice PubKey Buffer:", alicePk);
   const alicePkh = BITBOX.Crypto.hash160(alicePk);
-  console.log(alicePkh);
+  console.log("Alice PubKeyHash Buffer:", alicePkh);
   const aliceAddr = BITBOX.HDNode.toCashAddress(account);
-  console.log(aliceAddr);
+  console.log("Alice Address:", aliceAddr);
 
   const artifact = CashCompiler.compileFile(path.join(__dirname, 'SLPGenesis.cash'));
   const provider = new ElectrumNetworkProvider(NETWORK);
@@ -49,34 +61,72 @@ let minerFee = 2000;
   const contract = new Contract(artifact, [alicePkh], provider);
   console.log('contract address:', contract.address);
   const contractToSLPAddr = bchjs.SLP.Address.toSLPAddress(contract.address);
-  console.log(contractToSLPAddr);
+  console.log('contract SLP address:', contractToSLPAddr);
   const balance = await contract.getBalance();
   console.log('contract balance:', balance);
   const changeAmount = balance - (dust * 2) - minerFee;
-  console.log(changeAmount);
 
   console.log("OP Count", contract.opcount);
   console.log("Bytesize", contract.bytesize);
+  const contractUTXOs = await contract.getUtxos();
+//  console.log("getUtxos", contractUTXOs);
+
+  const mappedMintVout = contractUTXOs.find((obj) => {
+    if (obj.txid === no_0x_slp_tx_id && obj.vout === 2) {
+      return {
+        txid: obj.txid,
+        vout: parseInt(obj.vout),
+        satoshis: parseInt(obj.satoshis)
+      };
+    }
+  });
+
+  const mappedUploadVout = contractUTXOs.find((obj) => {
+    if (obj.txid === no_0x_frp_upload_txid && obj.vout === 1) {
+      return {
+        txid: obj.txid,
+        vout: parseInt(obj.vout),
+        satoshis: parseInt(obj.satoshis)
+      };
+    }
+  });
+
+  const mappedFundingVout = contractUTXOs.find((obj) => {
+    if (obj.satoshis >= 2000) {
+      return {
+        txid: obj.txid,
+        vout: parseInt(obj.vout),
+        satoshis: parseInt(obj.satoshis)
+      };
+    }
+  });
+
+  console.log("Mint VOut:", mappedMintVout);
+  console.log("Fund VOut:", mappedFundingVout);
 
   const remainder = balance - dust - dust - minerFee;
 
   const tx = await contract.functions
-
     .reclaim(alicePk, new SignatureTemplate(keypair))
-//    .to(aliceAddr, balance - 600)
-//    .meep()
+//    .to("bchtest:qzt6sz836wdwscld0pgq2prcpck2pssmwge9q87pe9", balance - 4500)
+//    .send()
 /*
+//  The "require(tx.hashOutputs..." statement fails to equate
+//  Temporarily disabling "genesisSLP" function
     .genesisSLP(
       alicePk,
       new SignatureTemplate(keypair),
       token_ticker,
       token_name,
       token_document_url,
+      token_document_hash,
       decimals,
       mint_baton_vout,
       initial_token_mint_quantity
     )
 */
+// GENESIS
+/*
     .withOpReturn([
       '0x534c5000',
       '0x01',
@@ -84,11 +134,49 @@ let minerFee = 2000;
       token_ticker,
       token_name,
       token_document_url,
-      '',                                         //document hash
+      token_document_hash,
       decimals,
       mint_baton_vout,
       initial_token_mint_quantity
     ])
+*/
+// MINT
+/*
+    .withOpReturn([
+      '0x534c5000',
+      '0x01',
+      'MINT',
+      slp_tx_id,
+      mint_baton_vout,
+      additional_token_mint_quantity
+    ])
+*/
+// FRP Upload
+/*
+    .withOpReturn([
+      '0x46525000',             // 'FRP\x00'
+      'UPLOAD',
+      'json',
+      'SLPGenesis',
+      'QmQG7aofWi4jvTFDng3xG8G1t6yCtMcBZP58LMDnyYYpA1',
+      '0x01'
+    ])
+*/
+// FRP Update
+/*
+    .withOpReturn([
+      '0x46525000',             // 'FRP\x00'
+      'UPDATE',
+      frp_upload_txid,          // Upload tx id
+      'cash',
+      'SLPGenesis',
+      'QmdBgKA8h3VA49M1Q952FUkVjGoKaHvbtLY8QZsYBLXriM',
+      '0x01'
+    ])
+*/
+//    .from(mappedMintVout)
+//    .from(mappedUploadVout)
+//    .from(mappedFundingVout)
     .to(contractToSLPAddr, dust)
     .to(contractToSLPAddr, dust)
 //    .to(contract.address, remainder)
